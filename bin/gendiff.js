3efb6f78932387c4139d434eeb2ad8815206c02e
Ivan <i_dont_known@mail.ru>
2022-03-17 11:33:49 +0300

    Modify gendiff.js functionality
    Now gendiff can take 2 arguments - paths to files to compare.
    Added option --format-type (-f) to specify output format.
    Fixed version to 0.0.2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,9 @@ const program = new Command;
 
 program
     .name('gendiff')
-    .version('60.0.0')
-    .description('Compares two configuration files and shows a difference.');
-
-program.parse();
+    .version('0.0.2')
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1>, <filepath2>')
+    .description('Compares two configuration files and shows a difference.')
+    // eslint-disable-next-line no-undef
+    .parse(process.argv);
