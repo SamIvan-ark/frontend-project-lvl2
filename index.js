00fc35950d2245a3950900b0cc9fb719fbca105c
Ivan Samozhenov <i_dont_known@mail.ru>
2022-09-07 20:39:00 +0300

    Move index.js, add correct entry point ./index.js (refacror 1.1)
    Logic of ./index.js now moved to src/index.js.
    ./index.js now import genDiff from src/index.js and exports it default.

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-import getDiff from './src/getDiff.js';
-import parseFile from './src/utils/parseFile.js';
-import formatDiff from './src/formatters/index.js';
-
-const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const diff = getDiff(parseFile(filepath1), parseFile(filepath2));
-
-  return formatDiff(diff, formatName);
-};
+import genDiff from './src/index.js';
 
 export default genDiff;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import getDiff from './getDiff.js';
+import parseFile from './utils/parseFile.js';
+import formatDiff from './formatters/index.js';
+
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const diff = getDiff(parseFile(filepath1), parseFile(filepath2));
+
+  return formatDiff(diff, formatName);
+};
+
+export default genDiff;
