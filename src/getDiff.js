aadde56aec4bea47965449ae0ee4bf35266ac0d8
Ivan Samozhenov <i_dont_known@mail.ru>
2022-09-15 20:04:05 +0300

    Reduce was replaced by map in src/getDiff.js (Refactor 1.2.2)

--- src/getDiff.js
+++ src/getDiff.js
@@ -8,45 +8,45 @@ const getDiff = (data1, data2) => {
     const keysUnion = _.union(arr1, arr2);
     const allKeysSorted = _.sortBy(keysUnion);
 
-    const diff = allKeysSorted.reduce((acc, key) => {
+    const diff = allKeysSorted.map((key) => {
       if (
         Object.prototype.hasOwnProperty.call(obj1, key)
         && Object.prototype.hasOwnProperty.call(obj2, key)
       ) {
         if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-          return [...acc, {
+          return {
             name: key,
             valueStatus: 'object',
             children: iter(obj1[key], obj2[key]),
-          }];
+          };
         }
         if (obj1[key] === obj2[key]) {
-          return [...acc, {
+          return {
             name: key,
             valueStatus: 'equial',
             value: obj2[key],
-          }];
+          };
         }
-        return [...acc, {
+        return {
           name: key,
           valueStatus: 'changed',
           value: [obj1[key], obj2[key]],
-        }];
+        };
       }
 
       if (Object.prototype.hasOwnProperty.call(obj1, key)) {
-        return [...acc, {
+        return {
           name: key,
           valueStatus: 'deleted',
           value: obj1[key],
-        }];
+        };
       }
-      return [...acc, {
+      return {
         name: key,
         valueStatus: 'added',
         value: obj2[key],
-      }];
-    }, []);
+      };
+    });
 
     return diff;
   };
