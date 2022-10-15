0816155dea752b80341a6e7bdf0cda54f8f111fd
Ivan Samozhenov <i_dont_known@mail.ru>
2022-10-15 15:32:51 +0300

    Refactor tests (Refactor 1.5)
    Improved testing logic and removed duplication of tests (beforeAll, testEach).
    Renamed all fixtures files to file-before*, file-after*.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,47 +2,23 @@
 import gendiff from '../index';
 import readFile from '../src/utils/readFile';
 
-const getFixturePath = (fileName) => `__fixtures__/${fileName}`;
+let resultStylish;
+let resultPlain;
 
-const genTestData = (filepath1, filepath2, rightFilePath, format) => {
-  const path1 = getFixturePath(filepath1);
-  const path2 = getFixturePath(filepath2);
-  const diff = gendiff(path1, path2, format);
-  const diffType = typeof (diff);
-  const right = readFile(getFixturePath(rightFilePath));
-
-  return [diff, diffType, right];
-};
-
-describe('compare recursively files json', () => {
-  const [diff, diffType, right] = genTestData('file-recursive1.json', 'file-recursive2.json', 'stylish-right.txt');
-
-  test('output type is must be string', () => {
-    expect(diffType).toBe('string');
-  });
-  test('output is must be as expected', () => {
-    expect(diff).toBe(right);
-  });
+beforeAll(() => {
+  resultStylish = readFile('__fixtures__/resultStylish.txt');
+  resultPlain = readFile('__fixtures__/resultPlain.txt');
 });
 
-describe('compare recursively files yml', () => {
-  const [diff, diffType, right] = genTestData('file-recursive1.yml', 'file-recursive2.yml', 'stylish-right.txt');
+const extensions = ['.yml', '.json'];
 
-  test('output type is must be string', () => {
-    expect(diffType).toBe('string');
-  });
-  test('output is must be as expected', () => {
-    expect(diff).toBe(right);
-  });
-});
+describe('Result must be as expected at stylish and plain output format. JSON output format is parsed without errors', () => {
+  test.each(extensions)('Compare files with %p extension', (extension) => {
+    const pathToFileBefore = `__fixtures__/file-before${extension}`;
+    const pathToFileAfter = `__fixtures__/file-after${extension}`;
 
-describe('compare plain output format', () => {
-  const [diff, diffType, right] = genTestData('file-recursive1.json', 'file-recursive2.yml', 'plain-right.txt', 'plain');
-
-  test('output type is must be string', () => {
-    expect(diffType).toBe('string');
-  });
-  test('output is must be as expected', () => {
-    expect(diff).toBe(right);
+    expect(gendiff(pathToFileBefore, pathToFileAfter)).toBe(resultStylish);
+    expect(gendiff(pathToFileBefore, pathToFileAfter, 'plain')).toBe(resultPlain);
+    expect(() => JSON.parse(gendiff(pathToFileBefore, pathToFileAfter, 'json'))).not.toThrow();
   });
 });
