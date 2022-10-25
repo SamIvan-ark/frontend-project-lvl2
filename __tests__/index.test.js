0c6b53d7e4fb04a91204ff73e14a755a700ad672
Ivan Samozhenov <i_dont_known@mail.ru>
2022-10-25 18:14:47 +0300

    Add testing case with obvious pass stylish argument (Refactor 3.1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,13 +9,16 @@ const resultPlain = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__/re
 
 const extensions = ['.yml', '.json'];
 
-describe('Result must be as expected at stylish and plain output format. JSON output format is parsed without errors', () => {
+describe(`Result must be as expected at stylish and plain output format. 
+  Case without passing output format returns stylish. 
+  JSON output format is parsed without errors.`, () => {
   test.each(extensions)('Compare files with %p extension', (extension) => {
     const pathToFileBefore = `__fixtures__/file-before${extension}`;
     const pathToFileAfter = `__fixtures__/file-after${extension}`;
 
-    expect(gendiff(pathToFileBefore, pathToFileAfter)).toBe(resultStylish);
+    expect(gendiff(pathToFileBefore, pathToFileAfter, 'stylish')).toBe(resultStylish);
     expect(gendiff(pathToFileBefore, pathToFileAfter, 'plain')).toBe(resultPlain);
+    expect(gendiff(pathToFileBefore, pathToFileAfter)).toBe(resultStylish);
     expect(() => JSON.parse(gendiff(pathToFileBefore, pathToFileAfter, 'json'))).not.toThrow();
   });
 });
